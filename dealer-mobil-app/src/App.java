f3866ef3b03d2ac57d833e583c72f4a19e5aed24
audyzahra <audyzahra21@gmail.com>
2025-12-27 07:39:38 +0700

    Setup untuk menjalankan apk

--- dealer-mobil-app/src/App.java
+++ dealer-mobil-app/src/App.java
@@ -1,5 +1,23 @@
+import javax.swing.UIManager;
+import javax.swing.SwingUtilities;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import view.LoginFrame;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        // Set Look and Feel
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Jalankan aplikasi dari LOGIN (2-TIER)
+        SwingUtilities.invokeLater(() -> {
+            new LoginFrame().setVisible(true);
+        });
     }
 }
